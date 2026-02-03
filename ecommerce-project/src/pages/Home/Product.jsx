3697ed8550800d920ad551043532b36530a39ec0
batuhanozatar <152212800+batuhanozatar@users.noreply.github.com>
2026-02-03 17:56:29 +0300

    test clicking add to cart button

--- ecommerce-project/src/pages/Home/Product.jsx
+++ ecommerce-project/src/pages/Home/Product.jsx
@@ -20,6 +20,7 @@ export function Product({product, loadCart}){
 <div className="product-container">
     <div className="product-image-container">
       <img className="product-image"
+        data-testid="product-image"
         src={product.image} />
     </div>
 
@@ -61,7 +62,8 @@ export function Product({product, loadCart}){
       Added
     </div>
 
-    <button className="add-to-cart-button button-primary" onClick={addToCart} >
+    <button className="add-to-cart-button button-primary"
+     data-testid="add-to-cart-button" onClick={addToCart} >
       Add to Cart
     </button>
   </div>
---
+++ ecommerce-project/src/pages/Home/Product.test.jsx
@@ -0,0 +1,61 @@
+import {it, expect, describe, vi} from 'vitest';
+import { Product } from './Product';
+import {render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+vi.mock('axios');
+import axios from 'axios';
+describe('Product component', ()=>{
+  it('displays the product details correctly', ()=>{
+    const product={
+      id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      image: "images/products/intermediate-composite-basketball.jpg",
+      name: "Intermediate Size Basketball",
+      rating: {
+        stars: 4,
+        count: 127
+      },
+      priceCents: 2095,
+      keywords: ["sports", "basketballs"]
+    };
+    const loadCart=vi.fn();
+    render(<Product product={product} loadCart={loadCart}/>);
+    expect(screen.getByText('Intermediate Size Basketball')).toBeInTheDocument();
+
+    expect(
+      screen.getByText('$20.95')
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByTestId('product-image')
+    ).toHaveAttribute('src', 'images/products/intermediate-composite-basketball.jpg');
+  });
+
+  it('adds a product to the cart', async ()=>{
+    const product={
+      id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      image: "images/products/intermediate-composite-basketball.jpg",
+      name: "Intermediate Size Basketball",
+      rating: {
+        stars: 4,
+        count: 127
+      },
+      priceCents: 2095,
+      keywords: ["sports", "basketballs"]
+    };
+    const loadCart=vi.fn();
+    render(<Product product={product} loadCart={loadCart}/>);
+
+    const user=userEvent.setup();
+    const addToCartButton= screen.getByTestId('add-to-cart-button');
+    await user.click(addToCartButton);
+
+    expect(axios.post).toHaveBeenCalledWith(
+      '/api/cart-items',
+      {
+        productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity:1
+      }
+    );
+    expect(loadCart).toHaveBeenCalled();
+  });
+});
